f525132466998a64eb5e1f2180c959c7978d7668
marissaapage <pag16005@byui.edu>
2021-04-24 20:49:39 -0600

    Portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
 const links = [
   {
     label: "Week1 notes",
-    url: "week1/index.html",
+    url: "/week1/index.html",
   },
   {
     label: "Week2 notes",
-    url: "week2/index.html",
+    url: "/week2/index.html",
+  },
+  {
+    label: "Week3 notes",
+    url: "/week3/index.html",
   },
 ];
 
